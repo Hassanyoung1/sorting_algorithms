5b6b4e6161dcfc1a4ba843a431dc3fb9922a185f
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-23 08:30:22 +1000

    bubble sort

--- sort.h
+++ sort.h
@@ -8,7 +8,7 @@
 
 
 
- /** data structure **/
+/** data structure **/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,9 +18,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -31,4 +31,4 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-#endif 
+#endif
