8a1967bd02436bf3c410f447ad6cec68791cf4f6
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-25 00:29:57 +1000

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an integer array in ascending order using
+ *                  the selection sort algorithm.
+ *
+ * @array: Pointer to the integer array to be sorted.
+ * @size:  Number of elements in the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t k, j;
+    int temp;
+    size_t lwest;
+
+    /* Iterate through the array */
+    for (k = 0; k < size - 1; k++)
+    {
+        /* Assume the current index 'i' contains the minimum value */
+        lwest = k;
+
+        /* Find the index of the minimum element in the unsorted part */
+        for (j = k + 1; j < size; j++)
+        {
+            if (array[j] < array[lwest])
+            {
+                /* Update the 'lowest' index if a smaller element is found */
+                lwest = j;
+            }
+        }
+
+        /* If 'lowest' index is different from 'i', swap the elements */
+        if (k != lwest)
+        {
+            temp = array[i];
+            array[i] = array[lwest];
+            array[lwest] = temp;
+            /* Print the current state of the array after swapping */
+            print_array(array, size);
+        }
+    }
+}
