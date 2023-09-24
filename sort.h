eef3588397fede562d1a1156f34e0e994595c643
MercifulMary <marybakare90@gmail.com>
2023-09-24 09:11:54 -0700

    update

--- sort.h
+++ sort.h
@@ -5,10 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-
-/** data structure **/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,17 +14,16 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-
-
-/** prototype  **/
-
+/* Function prototypes */
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
 #endif
