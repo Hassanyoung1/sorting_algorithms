d68e7d7fa2007b4d4c0e0888dbc1bc026354a890
MercifulMary <marybakare90@gmail.com>
2023-09-24 09:50:05 -0700

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,43 +10,44 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *nextNode;
+	listint_t *current, *nextNode;
 
-    if (list == NULL || *list == NULL)
-        return;
+	if (list == NULL || *list == NULL)
+		return;
 
-    current = *list;
-    while (current->next)
-    {
-        nextNode = current->next;
-        if (current->n > nextNode->n)
-        {
-            if (nextNode->next == NULL)
-                current->next = NULL;
-            else
-            {
-                current->next = nextNode->next;
-                nextNode->next->prev = nextNode->prev;
-                nextNode->next = NULL, nextNode->prev = NULL;
-            }
-            if (current->prev == NULL)
-            {
-                nextNode->next = current;
-                current->prev = nextNode;
-                *list = nextNode;
-            }
-            else
-            {
-                nextNode->next = current;
-                nextNode->prev = current->prev;
-                current->prev->next = nextNode;
-                current->prev = nextNode;
-            }
-            print_list(*list);
-            if (nextNode->n < nextNode->prev->n)
-                insertion_sort_list(list);
-        }
-        else
-            current = current->next;
-    }
+	current = *list;
+	while (current->next)
+	{
+	nextNode = current->next;
+	if (current->n > nextNode->n)
+	{
+		if (nextNode->next == NULL)
+			current->next = NULL
+		else
+		{
+		current->next = nextNode->next;
+		nextNode->next->prev = nextNode->prev;
+		nextNode->next = NULL;
+		nextNode->prev = NULL;
+		}
+		if (current->prev == NULL)
+		{
+		nextNode->next = current;
+		current->prev = nextNode;
+		*list = nextNode;
+		}
+		else
+		{
+		nextNode->next = current;
+		nextNode->prev = current->prev;
+		current->prev->next = nextNode;
+		current->prev = nextNode;
+		}
+		print_list(*list);
+		if (nextNode->n < nextNode->prev->n)
+		insertion_sort_list(list);
+	}
+	else
+		current = current->next;
+	}
 }
